b890b863db5e94260c905267f3368d36be0e5289
Moeez Muhammad <mmoeez48@gmail.com>
2024-06-10 23:42:11 -0400

    Add derivative for tensor contraction

--- tensor.c
+++ tensor.c
@@ -1,6 +1,7 @@
 #include "tensor.h"
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -17,6 +18,13 @@ void Tinit(Tensor *t, int dims[], int dim_len) {
 	}
 	t->n = t->dimszs[0];
 	t->arr = malloc(t->n * sizeof(double));
+	memset(t->arr, 0, t->n * sizeof(double));
+}
+// zero tensor of same shape as t
+Tensor* Tinitfrom(Tensor *t) {
+	Tensor *res = malloc(sizeof(Tensor));
+	Tinit(res, t->dims, t->dimlen);
+	return res;
 }
 void Tfree(Tensor *t) {
 	free(t->dims);
@@ -64,6 +72,29 @@ Tensor* Tadd(Tensor *A, Tensor *B) {
 	return res;
 }
 
+Tensor* tensorize_unary(Tensor* A, double (*func)(double)) {
+	Tensor *res = malloc(sizeof(Tensor));
+	Tinit(res, A->dims, A->dimlen);
+	for (int i = 0; i < A->n; i++) {
+		res[i] = func(A[i]);
+	}
+	return res;
+}
+
+Tensor* tensorize_binary(Tensor* A, Tensor* B, double (*func)(double, double)) {
+	if (CHECK_DIMS) {
+		assert(A->dimlen == B->dimlen);
+		for (int i = 0; i < A->dimlen; i++) {
+			assert(A->dims[i] == B->dims[i]);
+		}
+	}
+	Tensor *res = malloc(sizeof(Tensor));
+	Tinit(res, A->dims, A->dimlen);
+	for (int i = 0; i < A->n; i++) {
+		res[i] = func(A[i], B[i]);
+	}
+	return res;
+}
 // contract A and B along the innermost kai dimensions
 Tensor* Tcontract(Tensor *A, Tensor *B, int kai) {
 	if (CHECK_DIMS) {
@@ -77,7 +108,8 @@ Tensor* Tcontract(Tensor *A, Tensor *B, int kai) {
 	int Cdimlen = A->dimlen + B->dimlen - 2*kai;
 	int *Cdims = malloc(Cdimlen * sizeof(int));
 	for (int i = 0; i < A->dimlen - kai; i++) {
-		Cdims[i] = A->dims[A->dimlen - i - 1];
+//		Cdims[i] = A->dims[A->dimlen - i - 1];
+		Cdims[i] = A->dims[i];
 	}
 	for (int i = 0; i < B->dimlen - kai; i++) {
 		Cdims[A->dimlen-kai+i] = B->dims[kai+i];
@@ -102,14 +134,88 @@ Tensor* Tcontract(Tensor *A, Tensor *B, int kai) {
 	return C;
 }
 
+// derivative of tensor contraction w.r.t either A or B (determined by wrtA
+Tensor* Tcontract_D(Tensor *A, Tensor *B, int kai, bool wrtA) {
+	// idea is to do a similar nested 3 for loops to Tcontract
+	// and compute Ci, Ai, Bi
+	// with Ci/(Ai,Bi), we can easily compute the relevant index in the derivative (e.g. Ci * some_dimszs + Ai)
+	// and then (del Ci)/(del Ai) is the corresponding Bi
+	// not sure if there's a cleaner/more efficient way to do this
+	Tensor *D = malloc(sizeof(Tensor));
+	int Ddimlen = A->dimlen + B->dimlen - 2*kai; // C part
+	int *Ddims;
+	if (wrtA) {
+		Ddimlen += A->dimlen;
+	}
+	else {
+		Ddimlen += B->dimlen;
+	}
+	Ddims = malloc(Ddimlen * sizeof(int));
+	for (int i = 0; i < A->dimlen - kai; i++) {
+		//Ddims[i] = A->dims[A->dimlen - i - 1];
+		Ddims[i] = A->dims[i];
+	}
+	for (int i = 0; i < B->dimlen - kai; i++) {
+		Ddims[A->dimlen - kai + i] = B->dims[kai + i];
+	}
+	if (wrtA) {
+		for (int i  = 0; i < A->dimlen; i++) {
+			Ddims[A->dimlen + B->dimlen - 2*kai + i] = A->dims[i];
+		}
+	}
+	else {
+		for (int i  = 0; i < B->dimlen; i++) {
+			Ddims[A->dimlen + B->dimlen - 2*kai + i] = B->dims[i];
+		}
+	}
+	printf("==\n");
+	for (int i = 0; i < Ddimlen; i++) {
+		printf("%d ", Ddims[i]);
+	}
+	printf("\n");
+	printf("==\n");
+	Tinit(D, Ddims, Ddimlen);
+	free(Ddims);
+	int Ai, Bi, Ci, Di;
+	for (int i = 0; i < (A->dimszs[0]/A->dimszs[A->dimlen - kai]); i++) {
+		for (int j = 0; j < B->dimszs[kai]; j++) {
+			Ci = i * B->dimszs[kai] + j;
+			for (int k = 0; k < A->dimszs[A->dimlen - kai]; k++) {
+				Ai = i * A->dimszs[A->dimlen - kai] + k;
+				Bi = k * B->dimszs[kai] + j;
+				if (wrtA) {
+					Di = Ci * A->dimszs[0] + Ai;
+					D->arr[Di] = B->arr[Bi];
+				}
+				else {
+					Di = Ci * B->dimszs[0] + Bi;
+					D->arr[Di] = A->arr[Ai];
+				}
+			}
+		}
+	}
+	return D;
+}
+
 void Tprint(Tensor *t) {
-	if (t->dimlen <= 1 || t->dimlen > 2) {
+	if (t->dimlen <= 1 || t->dimlen > 3) {
 		for (int i = 0; i < t->n; i++) printf("%f ", t->arr[i]);
+	}
+	else if (t->dimlen == 2) {
+		for (int i = 0; i < t->dims[0]; i++) {
+			for (int j = 0; j < t->dims[1]; j++) {
+				printf("%f ", t->arr[i * t->dimszs[1] + j]);
+			}
+			printf("\n");
+		}
 	}
 	else {
 		for (int i = 0; i < t->dims[0]; i++) {
 			for (int j = 0; j < t->dims[1]; j++) {
-				printf("%f ", t->arr[i * t->dimszs[1] + j]);
+				for (int k = 0; k < t->dims[2]; k++) {
+					printf("%f ", t->arr[i*t->dimszs[1] + j*t->dimszs[2] + k]);
+				}
+				printf("\n");
 			}
 			printf("\n");
 		}
@@ -126,18 +232,26 @@ void test_tensor() {
 	Tensor* res = Tcontract(&tscal1, &tscal2, 0);
 	Tprint(res);
 	printf("\n");
+	Tensor *deriv = Tcontract_D(&tscal1, &tscal2, 0, false);
+	Tprint(deriv);
+	printf("\n");
 
 	Tensor t1;
 	Tensor t2;
-	int t1dims[] = {2, 2};
+	int t1dims[] = {3, 2};
 	int t2dims[] = {2};
 	Tinit(&t1, t1dims, 2);
 	Tinit(&t2, t2dims, 1);
 	t1.arr[0] = 2; t1.arr[1] = 3; t1.arr[2] = 1; t1.arr[3] = 5;
+	t1.arr[4] = 7; t1.arr[5] = 4;
 	t2.arr[0] = 1; t2.arr[1] = 3;
 	Tprint(&t1);
 	printf("\n");
 	Tensor *res2 = Tcontract(&t1, &t2, 1);
 	Tprint(res2);
+	printf("\n");
+	Tensor *deriv2 = Tcontract_D(&t1, &t2, 1, false);
+	printf("%d\n", deriv2->dimlen);
+	Tprint(deriv2);
 	printf("\n");
 }
--- tensor.h
+++ tensor.h
@@ -13,11 +13,16 @@ typedef struct {
 } Tensor;
 
 void Tinit(Tensor *t, int dims[], int dim_len);
+Tensor* Tinitfrom(Tensor *t);
 void Tfree(Tensor *t);
 void Tnext(Tensor *t, int idxs[]);
 void Tjump(Tensor *t, int *i, int idxs[], int dim_i, int jmp_amt);
-Tensor* Tadd(Tensor *A, Tensor *B);
+// applies a scalar function pointwise to a tensor
+Tensor* tensorize_unary(Tensor* A, double (*func)(double));
+Tensor* tensorize_binary(Tensor* A, Tensor* B, double (*func)(double, double));
 Tensor* Tcontract(Tensor *A, Tensor *B, int kai);
+// derivative of tensor contraction w.r.t either A or B (determined by wrtA
+Tensor* Tcontract_D(Tensor *A, Tensor *B, int kai, bool wrtA);
 void Tprint(Tensor *t);
 void test_tensor();
 #endif
